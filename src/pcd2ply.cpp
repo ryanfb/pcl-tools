dd6b33c3538ef7122809914cf91198024e4a8c70
Ryan Baumann <ryan.baumann@gmail.com>
2012-01-25 12:09:04 -0500

    Default to binary output

--- src/pcd2ply.cpp
+++ src/pcd2ply.cpp
@@ -6,21 +6,22 @@
 int
 main (int argc, char** argv)
 {
-  bool binary = false;
+  bool binary = true;
 
   if(argc < 3) {
     std::cerr << "Usage:" << std::endl;
-    std::cerr << argv[0] << "[-b] input.pcd output.ply" << std::endl;
+    std::cerr << argv[0] << " [-a] input.pcd output.ply" << std::endl;
+    std::cerr << "\t-a\tASCII output" << std::endl;
     return (1);
   }
 
   if(argc == 4) {
-    if(strncmp(argv[1],"-b",2) != 0) {
+    if(strncmp(argv[1],"-a",2) != 0) {
       std::cerr << "Error: unknown option!" << std::endl;
       return (1);
     }
     else {
-      binary = true;
+      binary = false;
       argv += 1;
     }
   }
